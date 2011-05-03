3917c31ffbaa6076387f5811eb1ed36facd73991
JP Hastings-Spital <jphastings@gmail.com>
2011-05-03 23:30:05 +0100

    Fixed environment type

--- environment.rb
+++ environment.rb
@@ -8,5 +8,5 @@ configure do
   $LOAD_PATH.unshift("#{File.dirname(__FILE__)}/lib/models")
   Dir.glob("#{File.dirname(__FILE__)}/lib/models/*.rb") { |lib| require File.basename(lib, '.*') }
   
-  ActiveRecord::Base.establish_connection(YAML::load(File.open("#{File.dirname(__FILE__)}/config/database.yml"))[ENV['ENVIRONMENT'] || 'development'])
+  ActiveRecord::Base.establish_connection(YAML::load(File.open("#{File.dirname(__FILE__)}/config/database.yml"))[ENV['RACK_ENV']])
 end
